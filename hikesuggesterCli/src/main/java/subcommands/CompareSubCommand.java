e0dc869abddd8cf6d97f542dd1d8c7f0421ce311
rachel <rachelcope.m@gmail.com>
2022-03-30 12:01:34 -0600

    Updated class fields of CompareSubCommand to be paramatized with Optional<>, save any fields that extended Collection. Updated logic of setCompareQuery() thereof. Created test method in CompareSubCommandTest class itInitializesClassFields(). Test passes.

--- hikesuggesterCli/src/main/java/subcommands/CompareSubCommand.java
+++ hikesuggesterCli/src/main/java/subcommands/CompareSubCommand.java
@@ -8,17 +8,18 @@ import database.models.DatabaseConnection;
 import picocli.CommandLine;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 @CommandLine.Command(name = "compare", mixinStandardHelpOptions = true)
 public class CompareSubCommand implements Runnable {
-    @CommandLine.Parameters(index = "0", description = "First argument, mountain name")
-    String mountainName1;
-    @CommandLine.Parameters(index = "1", description = "First argument, route name")
-    String routeName1;
-    @CommandLine.Parameters(index = "2", description = "Second argument, mountain name")
-    String mountainName2;
-    @CommandLine.Parameters(index = "3", description = "Second argument, route name")
-    String routeName2;
+    @CommandLine.Option(names = {"-m1", "--mountain1"}, description = "First argument, mountain name")
+    Optional<String> mountainName1;
+    @CommandLine.Option(names = {"-r1", "--route1"}, description = "First argument, route name")
+    Optional<String> routeName1;
+    @CommandLine.Option(names = {"-m2", "--mountain2"}, description = "Second argument, mountain name")
+    Optional<String> mountainName2;
+    @CommandLine.Option(names = {"-r2", "--route2"}, description = "Second argument, route name")
+    Optional<String> routeName2;
     @CommandLine.Option(names = {"-u", "--url"}, arity = "1..2",
             description = "Rather than entering in two separate Route and Mountain Names, enter two urls to compare.")
     public String[] routeUrls;
@@ -31,11 +32,15 @@ public class CompareSubCommand implements Runnable {
 
     private CompareQuery setCompareQuery() {
         CompareQuery query = new CompareQuery();
-        query.setMountainName1(mountainName1);
-        query.setRouteName1(routeName1);
-        query.setMountainName2(mountainName2);
-        query.setRouteName2(routeName2);
-        query.setRouteUrls(new ArrayList<>(Arrays.asList(routeUrls)));
+        if (routeUrls != null) {
+            query.setRouteUrls(new ArrayList<>(Arrays.asList(routeUrls)));
+        } else {
+            mountainName1.ifPresent(query::setMountainName1);
+            mountainName2.ifPresent(query::setMountainName2);
+            routeName1.ifPresent(query::setRouteName1);
+            routeName2.ifPresent(query::setRouteName1);
+
+        }
         return query;
     }
 }
--- hikesuggesterCli/src/test/java/subcommands/CompareSubCommandTest.java
+++ hikesuggesterCli/src/test/java/subcommands/CompareSubCommandTest.java
@@ -1,12 +1,35 @@
 package subcommands;
 
+import org.junit.Before;
 import org.junit.Test;
+import picocli.CommandLine;
+import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class CompareSubCommandTest {
+    int exitCode1, exitCode2;
+    CommandLine cmd1, cmd2;
+    CompareSubCommand mockApp1, mockApp2;
+
+    @Before
+    public void initData() {
+        mockApp1 = mock(CompareSubCommand.class);
+        cmd1 = new CommandLine(mockApp1);
+        exitCode1 = cmd1.execute("-m1", "Pike's Peak", "-m2", "Long's Peak", "-r1", "Barr Trail", "-r2", "KeyHole Route");
+
+        mockApp2 = mock(CompareSubCommand.class);
+        cmd2 = new CommandLine(mockApp2);
+        exitCode2 = cmd2.execute("--url", "https://www.14ers.com/route.php?route=elbe1", "https://www.14ers.com/route.php?route=mass1");
+    }
 
     @Test
-    public void itCreatesCompareQueryWithCliInput() {
+    public void itInitializesClassFields() {
+        assertThat(mockApp1.routeName1).usingRecursiveComparison().isEqualTo(Optional.of("Barr Trail"));
+        assertThat(mockApp1.routeName2).usingRecursiveComparison().isEqualTo(Optional.of("KeyHole Route"));
+        assertThat(mockApp1.mountainName1).usingRecursiveComparison().isEqualTo(Optional.of("Pike's Peak"));
+        assertThat(mockApp1.mountainName2).usingRecursiveComparison().isEqualTo(Optional.of("Long's Peak"));
 
+        assertThat(mockApp2.routeUrls).usingRecursiveComparison().isEqualTo(new String[]{"https://www.14ers.com/route.php?route=elbe1", "https://www.14ers.com/route.php?route=mass1"});
     }
 }
