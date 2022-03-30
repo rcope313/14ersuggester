bb78f6eff1093acac3e7b8c868b2b7c37277620d
rachel <rachelcope.m@gmail.com>
2022-03-30 11:20:01 -0600

    SuggestSubCommand and SearchSubCommand are pratically identical - updated class fields & setSearchCommand() to reflect SearchSubCommand. init data and test method of SuggestSubCommandTest class is a replica of SearchSubCommandTest class

--- hikesuggesterCli/src/main/java/subcommands/SuggestSubCommand.java
+++ hikesuggesterCli/src/main/java/subcommands/SuggestSubCommand.java
@@ -6,17 +6,13 @@ import database.models.DatabaseConnection;
 import database.models.SearchQuery;
 import picocli.CommandLine;
 import webscraper.MountainForecastScraper;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 @CommandLine.Command(name = "suggest", mixinStandardHelpOptions = true)
 public class SuggestSubCommand implements Runnable {
-    @CommandLine.Spec
-    CommandLine.Model.CommandSpec spec;
-    @CommandLine.Option(names = {"-v", "-verbose"},
-            description = "Verbose. Result output will yield all table columns, rather than just Mountain Name, Route Name, and URL.")
-    public boolean verbose = false;
+
     @CommandLine.Option(names = {"--m", "-mountainnames"}, split = ",",
             description = "Search by name of mountain. If searching by more than one mountain name, split list with ','.")
     public String[] mountainNames;
@@ -25,10 +21,10 @@ public class SuggestSubCommand implements Runnable {
     public String[] routeNames;
     @CommandLine.Option(names = {"--st", "-standardroute"},
             description = "Search if route is a standard route only.")
-    public boolean isStandardRoute = false;
+    public Optional<Boolean> isStandardRoute;
     @CommandLine.Option(names = {"--sn", "-snowroute"},
             description = "Search if route is a snow route only.")
-    public boolean isSnowRoute = false;
+    public Optional<Boolean> isSnowRoute;
     @CommandLine.Option(names = {"--g", "-grades"}, split = ",",
             description = "Search by grade of route. If searching by more than one grade, split list with ','.")
     public Integer[] grades;
@@ -41,37 +37,37 @@ public class SuggestSubCommand implements Runnable {
     public String[] trailheads;
     @CommandLine.Option(names = {"--ste", "-startelevation"},
             description = "Search by start elevation. Result will yield all results greater than or equal to input.")
-    public int startElevation;
+    public Optional<Integer> startElevation;
     @CommandLine.Option(names = {"--sue", "-summitelevation"},
             description = "Search by summit elevation. Result will yield all results greater than or equal to input.")
-    public int summitElevation;
+    public Optional<Integer> summitElevation;
     @CommandLine.Option(names = {"--tg", "-totalgain"},
             description = "Search by total gain. Result will yield all results greater than or equal to input.")
-    public int totalGain;
+    public Optional<Integer> totalGain;
     @CommandLine.Option(names = {"--rl", "-routelength"},
             description = "Search by length of route. Result will yield all results greater than or equal to input.")
-    public double routeLength;
+    public Optional<Double> routeLength;
     @CommandLine.Option(names = {"--ex", "-exposure"},
             description = "Search by exposure. " +
                     "Valid strings include: 'Low', 'Moderate', 'Considerable', 'High', and 'Extreme'.")
-    public String exposure;
+    public Optional<String> exposure;
     @CommandLine.Option(names = {"--rp", "-rockfallpotential"},
             description = "Search by rockfall potential. " +
                     "Valid strings include: 'Low', 'Moderate', 'Considerable', 'High', and 'Extreme'.")
-    public String rockfallPotential;
+    public Optional<String>  rockfallPotential;
     @CommandLine.Option(names = {"--rf", "-routefinding"},
             description = "Search by route finding. " +
                     "Valid strings include: 'Low', 'Moderate', 'Considerable', 'High', and 'Extreme'.")
-    public String routeFinding;
+    public Optional<String>  routeFinding;
     @CommandLine.Option(names = {"--ct", "-commitment"},
             description = "Search by commitment. " +
                     "Valid strings include: 'Low', 'Moderate', 'Considerable', 'High', and 'Extreme'.")
-    public String commitment;
+    public Optional<String>  commitment;
     @CommandLine.Option(names = {"--mr", "-mulitpleroutes"},
             description = "Search if there are multiple routes of varied elevation gain and route length. " +
                     "Meaning, the approach road may have winter closures or such that the road difficulty significantly increases. " +
                     "This will be noted as a '0' in the route length and total gain columns.")
-    public boolean hasMultipleRoutes = false;
+    public Optional<Boolean> hasMultipleRoutes;
     @CommandLine.Option(names = {"--ru", "-routeurls"}, split = ", ",
             description = "Search by route url. If searching by more than one url, split list with ','.")
     public String[] routeUrls;
@@ -84,33 +80,47 @@ public class SuggestSubCommand implements Runnable {
 
     @Override
     public void run() {
-//        spec.commandLine().getOut().println(setSearchQuery().createQuerySyntax());
-//        ** use to create tests
         CliSuggestOutput output = new CliSuggestOutput(new RoutesTrailheadsDao(new DatabaseConnection()), new MountainForecastScraper());
         output.buildCliTable(setSearchQuery());
     }
 
     private SearchQuery setSearchQuery() {
         SearchQuery query = new SearchQuery();
-        query.setMountainNames(new ArrayList<>(Arrays.asList(mountainNames)));
-        query.setRouteNames(new ArrayList<>(Arrays.asList(routeNames)));
-        query.setStandardRoute(isStandardRoute);
-        query.setSnowRoute(isSnowRoute);
-        query.setGrades(new ArrayList<>(Arrays.asList(grades)));
-        query.setGradeQualities(new ArrayList<>(Arrays.asList(gradeQualities)));
-        query.setTrailheads(new ArrayList<>(Arrays.asList(trailheads)));
-        query.setStartElevation(startElevation);
-        query.setSummitElevation(summitElevation);
-        query.setTotalGain(totalGain);
-        query.setRouteLength(routeLength);
-        query.setExposure(exposure);
-        query.setRockfallPotential(rockfallPotential);
-        query.setRouteFinding(routeFinding);
-        query.setCommitment(commitment);
-        query.setHasMultipleRoutes(hasMultipleRoutes);
-        query.setRouteUrls(new ArrayList<>(Arrays.asList(routeUrls)));
-        query.setRoadDifficulties(new ArrayList<>(Arrays.asList(roadDifficulties)));
-        query.setTrailheadUrls(new ArrayList<>(Arrays.asList(trailheadUrls)));
+        if (mountainNames != null) {
+            query.setMountainNames(new ArrayList<>(Arrays.asList(mountainNames)));
+        }
+        if (routeNames != null) {
+            query.setRouteNames(new ArrayList<>(Arrays.asList(routeNames)));
+        }
+        if (grades != null) {
+            query.setGrades(new ArrayList<>(Arrays.asList(grades)));
+        }
+        if (gradeQualities != null) {
+            query.setGradeQualities(new ArrayList<>(Arrays.asList(gradeQualities)));
+        }
+        if (trailheads != null) {
+            query.setTrailheads(new ArrayList<>(Arrays.asList(trailheads)));
+        }
+        if (routeUrls != null) {
+            query.setRouteUrls(new ArrayList<>(Arrays.asList(routeUrls)));
+        }
+        if (roadDifficulties != null) {
+            query.setRoadDifficulties(new ArrayList<>(Arrays.asList(roadDifficulties)));
+        }
+        if (trailheadUrls != null) {
+            query.setTrailheadUrls(new ArrayList<>(Arrays.asList(trailheadUrls)));
+        }
+        startElevation.ifPresent(query::setStartElevation);
+        summitElevation.ifPresent(query::setSummitElevation);
+        totalGain.ifPresent(query::setTotalGain);
+        routeLength.ifPresent(query::setRouteLength);
+        exposure.ifPresent(query::setExposure);
+        rockfallPotential.ifPresent(query::setRockfallPotential);
+        routeFinding.ifPresent(query::setRouteFinding);
+        commitment.ifPresent(query::setCommitment);
+        hasMultipleRoutes.ifPresent(query::setHasMultipleRoutes);
+        isSnowRoute.ifPresent(query::setSnowRoute);
+        isStandardRoute.ifPresent(query::setStandardRoute);
         return query;
     }
 
--- hikesuggesterCli/src/test/java/subcommands/SuggestSubCommandTest.java
+++ hikesuggesterCli/src/test/java/subcommands/SuggestSubCommandTest.java
@@ -1,4 +1,82 @@
 package subcommands;
 
+import org.junit.Before;
+import org.junit.Test;
+import picocli.CommandLine;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
 public class SuggestSubCommandTest {
+    int exitCode1, exitCode2, exitCode3, exitCode4, exitCode5, exitCode6, exitCode7, exitCode8, exitCode9;
+    CommandLine cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7, cmd8, cmd9;
+    SuggestSubCommand mockApp1, mockApp2, mockApp3, mockApp4, mockApp5, mockApp6, mockApp7, mockApp8, mockApp9;
+
+    @Before
+    public void initData() {
+        mockApp1 = mock(SuggestSubCommand.class);
+        cmd1 = new CommandLine(mockApp1);
+        exitCode1 = cmd1.execute();
+
+        mockApp2 = mock(SuggestSubCommand.class);
+        cmd2 = new CommandLine(mockApp2);
+        exitCode2 = cmd2.execute("--m", "Mt. Elbert", "--m", "Longs Peak", "--m", "Mt. Antero", "--st");
+
+        mockApp3 = mock(SuggestSubCommand.class);
+        cmd3 = new CommandLine(mockApp3);
+        exitCode3 = cmd3.execute( "--m", "Mt. Elbert", "--m", "Longs Peak", "--m", "Mt. Antero", "--tg", "5000", "--rl", "4.5");
+
+        mockApp4 = mock(SuggestSubCommand.class);
+        cmd4 = new CommandLine(mockApp4);
+        exitCode4 = cmd4.execute("--sn", "--r", "Box Creek Couloirs", "--r", "South Face");
+
+        mockApp5 = mock(SuggestSubCommand.class);
+        cmd5 = new CommandLine(mockApp5);
+        exitCode5 = cmd5.execute("--ste", "8000", "--sue", "14000");
+
+        mockApp6 = mock(SuggestSubCommand.class);
+        cmd6 = new CommandLine(mockApp6);
+        exitCode6 = cmd6.execute("-exposure", "low", "-routefinding", "low", "-rockfallpotential", "low", "-commitment", "low");
+
+        mockApp7 = mock(SuggestSubCommand.class);
+        cmd7 = new CommandLine(mockApp7);
+        exitCode7 = cmd7.execute("--g", "1", "--g", "2", "--g", "3", "--gq", "Easy", "--gq", "Difficult");
+
+        mockApp8 = mock(SuggestSubCommand.class);
+        cmd8 = new CommandLine(mockApp8);
+        exitCode8 = cmd8.execute( "-roaddifficulties", "1", "-roaddifficulties", "2", "-roaddifficulties", "3" );
+
+        mockApp9 = mock(SuggestSubCommand.class);
+        cmd9 = new CommandLine(mockApp9);
+        exitCode9 = cmd9.execute("-routeurls", "https://www.14ers.com/route.php?route=elbe1", "-routeurls", "https://www.14ers.com/route.php?route=elbe2", "-trailheadurls", "/trailheadsview.php?thparm=sw01", "-trailheadurls", "/trailheadsview.php?thparm=sw03");
+    }
+
+    @Test
+    public void itInitializesClassFields() {
+        assertThat(mockApp2.mountainNames).usingRecursiveComparison().isEqualTo(new String[]{"Mt. Elbert", "Longs Peak", "Mt. Antero"});
+        assertThat(mockApp2.isStandardRoute).usingRecursiveComparison().isEqualTo(Optional.of(true));
+
+        assertThat(mockApp3.mountainNames).usingRecursiveComparison().isEqualTo(new String[]{"Mt. Elbert", "Longs Peak", "Mt. Antero"});
+        assertThat(mockApp3.totalGain).usingRecursiveComparison().isEqualTo(Optional.of(5000));
+        assertThat(mockApp3.routeLength).usingRecursiveComparison().isEqualTo(Optional.of(4.5));
+
+        assertThat(mockApp4.routeNames).usingRecursiveComparison().isEqualTo(new String[]{"Box Creek Couloirs", "South Face"});
+        assertThat(mockApp4.isSnowRoute).usingRecursiveComparison().isEqualTo(Optional.of(true));
+
+        assertThat(mockApp5.startElevation).usingRecursiveComparison().isEqualTo(Optional.of(8000));
+        assertThat(mockApp5.summitElevation).usingRecursiveComparison().isEqualTo(Optional.of(14000));
+
+        assertThat(mockApp6.exposure).usingRecursiveComparison().isEqualTo(Optional.of("low"));
+        assertThat(mockApp6.routeFinding).usingRecursiveComparison().isEqualTo(Optional.of("low"));
+        assertThat(mockApp6.rockfallPotential).usingRecursiveComparison().isEqualTo(Optional.of("low"));
+        assertThat(mockApp6.commitment).usingRecursiveComparison().isEqualTo(Optional.of("low"));
+
+        assertThat(mockApp7.grades).usingRecursiveComparison().isEqualTo(new int[]{1,2,3});
+        assertThat(mockApp7.gradeQualities).usingRecursiveComparison().isEqualTo(new String[]{"Easy", "Difficult"});
+
+        assertThat(mockApp8.roadDifficulties).usingRecursiveComparison().isEqualTo(new int[]{1,2,3});
+
+        assertThat(mockApp9.trailheadUrls).usingRecursiveComparison().isEqualTo(new String[]{"/trailheadsview.php?thparm=sw01", "/trailheadsview.php?thparm=sw03"});
+        assertThat(mockApp9.routeUrls).usingRecursiveComparison().isEqualTo(new String[]{"https://www.14ers.com/route.php?route=elbe1", "https://www.14ers.com/route.php?route=elbe2"});
+    }
 }
